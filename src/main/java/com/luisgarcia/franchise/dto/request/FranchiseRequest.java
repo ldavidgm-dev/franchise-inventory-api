7ba54fc24b4faf9a53d7055dee463981844df0c0
ldavidgm-dev <ldavidgm97@gmail.com>
2026-04-28 09:28:07 -0500

    Creation of the Franchise endpoint
    - FranchiseController.java:  Add Franchise controller with POST endpoint to create franchises via FranchiseService and return created response with HTTP 201 status.
    - FranchiseRequest.java:  Add DTO class for Franchise request.
    - FranchiseResponse.java:  Add DTO classe for Franchise response to handle input validation and API output mapping.
    - Branch.java:  Define JPA entity for Branch with many-to-one relationship to Franchise and basic fields mapping for persistence.
    - Franchise.java:  Define JPA entity for Branch with many-to-one relationship to Franchise and basic fields mapping for persistence.
    - FranchiseRepository.java:  Create Spring Data JPA repository for Franchise entity to handle CRUD database operations.
    - FranchiseService.java:  Implement service layer for Franchise to handle business logic for creating a franchise and mapping request/response using FranchiseRepository.
    - application.properties: Configure PostgreSQL datasource and JPA settings for Spring Boot application with automatic schema update and SQL logging enabled.

---
+++ src/main/java/com/luisgarcia/franchise/dto/request/FranchiseRequest.java
@@ -0,0 +1,17 @@
+package com.luisgarcia.franchise.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class FranchiseRequest {
+    
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
